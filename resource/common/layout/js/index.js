50047c3092b78e9ac410d10bba3feb40bde35ae7
Adam <b9670094@gmail.com>
2017-08-05 06:17:23 +0800

    v1

--- resource/common/layout/js/index.js
+++ resource/common/layout/js/index.js
@@ -1,17 +1,18 @@
-$(document).ready(function() {
-	$('div>div>div').each(function(id) {
-		$(this).css({
-			position : 'relative',
-			top : '-200px',
-			opacity : 0
-		});
-		var wait = Math.floor((Math.random() * 3000) + 1);
-		$(this).delay(wait).animate({
-			top : '0px',
-			opacity : 1
-		}, 1000);
-	})
-});
+//# sourceURL=1.js
+
+$('button').trigger('click');
+$('div>div>div').each(function(id) {
+	$(this).css({
+		position : 'relative',
+		top : '-200px',
+		opacity : 0
+	});
+	var wait = Math.floor((Math.random() * 3000) + 1);
+	$(this).delay(wait).animate({
+		top : '0px',
+		opacity : 1
+	}, 1000);
+})
 
 function hello() {
 	$('div#install').hide();
